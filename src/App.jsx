926f2a0634471ee1687109aa558ca529811e1297
novakkaroly <novakkaroly81@gmail.com>
2025-10-19 22:09:12 +0100

    ready to start

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-9xl text-red-400">hi</div>;
+}
+
+export default App;
